601157ff00fac84a800028047cd49ab081b7957d
Maksim Surmach <m.surmach@godeltech.com>
2022-05-27 17:15:43 +0300

    Daily improvements 27.05

---
+++ src/main/java/com/youtube/amigoscode/springsecuritylearning/configuration/ApplicationSecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.youtube.amigoscode.springsecuritylearning.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+                authorizeHttpRequests().
+                anyRequest().
+                authenticated().
+                and().
+                httpBasic();
+    }
+}
---
+++ src/main/java/com/youtube/amigoscode/springsecuritylearning/model/Student/java/Student.java
@@ -0,0 +1,9 @@
+package com.youtube.amigoscode.springsecuritylearning.model.Student.java;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private final Integer studentId;
+    private final String studentName;
+}
